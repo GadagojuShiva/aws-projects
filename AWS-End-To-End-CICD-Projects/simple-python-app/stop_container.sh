7d3ebe7a6de7c65e3f87bb497dd716d0092b61e6
Gadagoju Shiva <133100130+GadagojuShiva@users.noreply.github.com>
2024-02-05 14:05:35 +0530

    Update stop_container.sh

--- AWS-End-To-End-CICD-Projects/simple-python-app/stop_container.sh
+++ AWS-End-To-End-CICD-Projects/simple-python-app/stop_container.sh
@@ -1,5 +1,18 @@
 #!/bin/bash
 set -e
 
-# Stop the running container (if any)
-echo "Hi"
+# Get the container ID using docker ps and awk
+CONTAINER_ID=$(docker ps | awk 'NR==2{print $1}')
+
+# Check if the container is running
+if [ -n "$CONTAINER_ID" ]; then
+    # Stop the Docker container
+    docker stop $CONTAINER_ID
+
+    # Remove the Docker container
+    docker rm $CONTAINER_ID
+
+    echo "Container $CONTAINER_ID stopped and removed successfully."
+else
+    echo "No running container found."
+fi
