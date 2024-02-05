575163d566b1577f70241dd5eb99b4bbdfce6d12
Gadagoju Shiva <133100130+GadagojuShiva@users.noreply.github.com>
2024-02-05 14:02:26 +0530

    Update start_container.sh

--- AWS-End-To-End-CICD-Projects/simple-python-app/start_container.sh
+++ AWS-End-To-End-CICD-Projects/simple-python-app/start_container.sh
@@ -2,7 +2,7 @@
 set -e
 
 # Pull the Docker image from Docker Hub
-echo
+docker pull docker pull gadagojushiva/simple-python-app
 
 # Run the Docker image as a container
-echo
+docker run -d -p 5000:5000
